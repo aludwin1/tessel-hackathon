40f5dfe2643c871b771148ef64067c664eba80b6
Almendra <estrada.almendra@gmail.com>
2018-08-14 15:57:37 -0400

    cam

---
+++ camera/camera.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Import the interface to Tessel hardware
+const tessel = require('tessel');
+var av = require('tessel-av');
+var os = require('os');
+var http = require('http');
+var port = 8000;
+var camera = new av.Camera();
+
+http
+  .createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'image/jpg' });
+
+    camera.capture().pipe(response);
+  })
+  .listen(port, () => console.log(`71.190.247.98.local:8000`));
